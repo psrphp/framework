fef87a0d8cb68a9e9b2223384844a9c37af95364
psrphp <96121255+psrphp@users.noreply.github.com>
2023-05-26 14:32:34 +0800

    Create Widget.php

---
+++ src/Widget.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PsrPHP\Framework;
+
+use Composer\Autoload\ClassLoader;
+use Exception;
+use InvalidArgumentException;
+use PsrPHP\Template\Template;
+use ReflectionClass;
+
+class Widget
+{
+    private $template;
+
+    public function __construct(
+        Template $template
+    ) {
+        $this->template = $template;
+    }
+
+    public function get(string $key): string
+    {
+        $widget_file = $this->parseKey($key);
+        if (!$widget_file || !file_exists($widget_file)) {
+            throw new Exception('not found widget [' . $key . '].');
+        }
+
+        return $this->template->renderFromString(file_get_contents($widget_file));
+    }
+
+    private function parseKey(string $key): ?string
+    {
+        list($filename, $group) = explode('@', $key . '@');
+
+        if (!strlen($filename)) {
+            throw new InvalidArgumentException('Invalid Argument Exception');
+        }
+
+        if (!strlen($group)) {
+            $root = dirname(dirname(dirname((new ReflectionClass(ClassLoader::class))->getFileName())));
+            return $root . '/widget/' . $filename . '.php';
+        }
+
+        $group = str_replace('.', '/', $group);
+        if (!isset(Framework::getAppList()[$group])) {
+            return null;
+        }
+
+        $class_name = str_replace(['-', '/'], ['', '\\'], ucwords('App\\' . $group . '\\App', '/\\-'));
+        $reflector = new ReflectionClass($class_name);
+        return dirname(dirname($reflector->getFileName())) . '/widget/' . $filename . '.php';
+    }
+}
